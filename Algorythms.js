12e27e5460c97a9591c7537b1cf9c8a78b1df7ed
Ilya Grafkin <95228082+Orme-g@users.noreply.github.com>
2025-05-01 02:11:22 +0300

    Update Algorythms.js

--- Algorythms.js
+++ Algorythms.js
@@ -6,6 +6,7 @@ function singleNumber(nums) {
     let obj = {}
     nums.forEach((item) => {
         obj[item] = obj[item] ? 0 : 1
+        // obj[item] = (item in obj) ? 0 : 1
     })  // Here for each array item we create field in obj. Field's value will be: 0 if item repeated; 1 if item didn't repeate.
     for(let [key, value] of Object.entries(obj)) {
         if(value === 1) {  // If item didn't repeat we push it to result.
