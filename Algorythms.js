efdeff2a9e79c240fd88ac1c3fe2deaddbe9ac2f
Ilya Grafkin <95228082+Orme-g@users.noreply.github.com>
2025-04-30 16:07:02 +0300

    Update Algorythms.js

--- Algorythms.js
+++ Algorythms.js
@@ -1,1 +1,16 @@
+// Find repeated/not repeated values in array (avoiding Set).
 
+let nums = [2,1,2,3,4,1]
+function singleNumber(nums) {
+    let result = []
+    let obj = {}
+    nums.forEach((item) => {
+        obj[item] = obj[item] ? 0 : 1
+    })  // Here for each array item we create field in obj. Field's value will be: 0 if item repeated; 1 if item didn't repeate.
+    for(let [key, value] of Object.entries(obj)) {
+        if(value === 1) {  // If item didn't repeat we push it to result.
+            result.push(key)
+        }
+    }
+    return result
+};
